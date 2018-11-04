012c78119baf3b5e99d2da8c4f60a062f5c1602b
Frank Guowei Yang <frankhome61@hotmail.com>
2018-11-03 19:17:34 -0700

    starting on vision

---
+++ ios/Swift/imagepicker/ImagePickerViewController.swift
@@ -0,0 +1,246 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import UIKit
+import SwiftyJSON
+
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    let imagePicker = UIImagePickerController()
+    let session = URLSession.shared
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    @IBOutlet weak var labelResults: UITextView!
+    @IBOutlet weak var faceResults: UITextView!
+    
+    var googleAPIKey = "AIzaSyC-yPYDVhHsKp7d3AX5om0HO15UabTeoIo"
+    var googleURL: URL {
+        return URL(string: "https://vision.googleapis.com/v1/images:annotate?key=\(googleAPIKey)")!
+    }
+    
+    @IBAction func loadImageButtonTapped(_ sender: UIButton) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
+        labelResults.isHidden = true
+        faceResults.isHidden = true
+        spinner.hidesWhenStopped = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+
+/// Image processing
+
+extension ViewController {
+    
+    func analyzeResults(_ dataToParse: Data) {
+        
+        // Update UI on the main thread
+        DispatchQueue.main.async(execute: {
+            
+            
+            // Use SwiftyJSON to parse results
+            let json = JSON(data: dataToParse)
+            let errorObj: JSON = json["error"]
+            
+            self.spinner.stopAnimating()
+            self.imageView.isHidden = true
+            self.labelResults.isHidden = false
+            self.faceResults.isHidden = false
+            self.faceResults.text = ""
+            
+            // Check for errors
+            if (errorObj.dictionaryValue != [:]) {
+                self.labelResults.text = "Error code \(errorObj["code"]): \(errorObj["message"])"
+            } else {
+                var textArray: [TextBox] = []
+                var resultArray: [[TextBox]] = []
+                let responses: JSON = json["responses"][0]["textAnnotations"]
+                let responseArray = responses.array
+                
+                
+                for i in 1...responseArray!.count-1 {
+                    let responseDict = responseArray![i]
+                //}
+                //for responseDict in responseArray! {
+                    let boundingPolyVerticeLst = responseDict["boundingPoly"]["vertices"].array
+                    let text = responseDict["description"].string as! NSString
+                    print(boundingPolyVerticeLst)
+                    print(text)
+                    var textBox = TextBox(jsonPtLst: boundingPolyVerticeLst, text: text)
+                    textArray.append(textBox)
+                }
+                textArray.sort(by: {$0.getY() < $1.getY() })
+                print(textArray)
+                print(self.groupTextBox(textArray: textArray))
+            }
+        })
+        
+    }
+    
+    func groupTextBox(textArray: [TextBox]) -> [[TextBox]] {
+        var res: [[TextBox]] = []
+        var currY = textArray[0].getY()
+        var currMargin = textArray[0].getMargin()
+        var regionThreshold: Double = Double(currMargin) / 2.0
+        var currRes: [TextBox] = []
+        for textBox in textArray {
+            var vDist = abs(currY - textBox.getY())
+            if (Double(vDist) <= Double(regionThreshold)) {
+                currRes.append(textBox)
+            } else {
+                res.append(currRes)
+                currRes = [textBox]
+                currY = textBox.getY()
+                currMargin = textBox.getMargin()
+            }
+        }
+        return res
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFit
+            imageView.isHidden = true // You could optionally display the image here by setting imageView.image = pickedImage
+            spinner.startAnimating()
+            faceResults.isHidden = true
+            labelResults.isHidden = true
+            
+            // Base64 encode the image and create the request
+            let binaryImageData = base64EncodeImage(pickedImage)
+            createRequest(with: binaryImageData)
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func resizeImage(_ imageSize: CGSize, image: UIImage) -> Data {
+        UIGraphicsBeginImageContext(imageSize)
+        image.draw(in: CGRect(x: 0, y: 0, width: imageSize.width, height: imageSize.height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        let resizedImage = UIImagePNGRepresentation(newImage!)
+        UIGraphicsEndImageContext()
+        return resizedImage!
+    }
+}
+
+
+/// Networking
+
+extension ViewController {
+    func base64EncodeImage(_ image: UIImage) -> String {
+        var imagedata = UIImagePNGRepresentation(image)
+        
+        // Resize the image if it exceeds the 2MB API limit
+        if (imagedata?.count > 2097152) {
+            let oldSize: CGSize = image.size
+            let newSize: CGSize = CGSize(width: 800, height: oldSize.height / oldSize.width * 800)
+            imagedata = resizeImage(newSize, image: image)
+        }
+        
+        return imagedata!.base64EncodedString(options: .endLineWithCarriageReturn)
+    }
+    
+    func createRequest(with imageBase64: String) {
+        // Create our request URL
+        
+        var request = URLRequest(url: googleURL)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue(Bundle.main.bundleIdentifier ?? "", forHTTPHeaderField: "X-Ios-Bundle-Identifier")
+        
+        // Build our API request
+        let jsonRequest = [
+            "requests": [
+                "image": [
+                    "content": imageBase64
+                ],
+                "features": [
+                    [
+                        "type": "TEXT_DETECTION",
+                        "maxResults": 10
+                    ]
+                ]
+            ]
+        ]
+        let jsonObject = JSON(jsonDictionary: jsonRequest)
+        
+        // Serialize the JSON
+        guard let data = try? jsonObject.rawData() else {
+            return
+        }
+        
+        request.httpBody = data
+        
+        // Run the request on a background thread
+        DispatchQueue.global().async { self.runRequestOnBackgroundThread(request) }
+    }
+    
+    func runRequestOnBackgroundThread(_ request: URLRequest) {
+        // run the request
+        
+        let task: URLSessionDataTask = session.dataTask(with: request) { (data, response, error) in
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "")
+                return
+            }
+            
+            self.analyzeResults(data)
+        }
+        
+        task.resume()
+    }
+}
+
+
+// FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
+// Consider refactoring the code to use the non-optional operators.
+fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+    switch (lhs, rhs) {
+    case let (l?, r?):
+        return l < r
+    case (nil, _?):
+        return true
+    default:
+        return false
+    }
+}
+
+// FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
+// Consider refactoring the code to use the non-optional operators.
+fileprivate func > <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+    switch (lhs, rhs) {
+    case let (l?, r?):
+        return l > r
+    default:
+        return rhs < lhs
+    }
+}
---
+++ ios/Swift/imagepicker/TextBox.swift
@@ -0,0 +1,49 @@
+//
+//  TextBox.swift
+//  imagepicker
+//
+//  Created by Frank on 11/3/18.
+//  Copyright © 2018 Sara Robinson. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class TextBox: NSObject {
+    var content: NSString
+    var margin: Int
+    var p1: [Int]
+    var p2: [Int]
+    var p3: [Int]
+    var p4: [Int]
+    
+    init(jsonPtLst: [JSON]?, text: NSString) {
+        self.content = text
+        let p1Json = jsonPtLst![0]
+        let p2Json = jsonPtLst![1]
+        let p3Json = jsonPtLst![2]
+        let p4Json = jsonPtLst![3]
+        self.p1 = [p1Json["x"].int, p1Json["y"].int] as! [Int]
+        self.p2 = [p2Json["x"].int, p2Json["y"].int] as! [Int]
+        self.p3 = [p3Json["x"].int, p3Json["y"].int] as! [Int]
+        self.p4 = [p4Json["x"].int, p4Json["y"].int] as! [Int]
+        self.margin = self.p4[1] - self.p1[1]
+    }
+    
+    func getX() -> Int {
+        return self.p1[0]
+    }
+    
+    func getY() -> Int {
+        return self.p1[1]
+    }
+    
+    func getMargin() -> Int {
+        return self.margin
+    }
+    
+    public override var description: String {
+        return (self.content as String) + " " + "(" + self.p1[0] + ", " + self.p1[1] + ")"
+    }
+    
+}
